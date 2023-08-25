a8f5103ca1a631c8b5b1716e3e724f3c0a737496
marmarsol4 <marmarsol4@alum.us.es>
2023-08-25 17:34:04 +0200

    Fix in URL regarding CORS
    Co-authored-by: Ismael Barrera García <TerryIsmael@users.noreply.github.com>

--- deno_backend/src/server.ts
+++ deno_backend/src/server.ts
@@ -4,12 +4,12 @@ import connectDB from "./utils/connectDB.ts";
 
 const app = new Application();
 export const client = await connectDB();
-
+  
 app.use(async (ctx, next) => {
-    ctx.response.headers.set('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+    ctx.response.headers.set('Access-Control-Allow-Origin', '*');
     ctx.response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     ctx.response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-     await next();
+    await next();
   });
 
 app.use(router.routes());
