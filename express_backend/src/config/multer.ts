36cabb6e9bf194eced15a336e8e6d6864e4e560d
ismbargar <ismbargar@alum.us.es>
2024-06-01 16:52:57 +0200

    Más funciones de workspace añadidas

--- express_backend/src/config/multer.ts
+++ express_backend/src/config/multer.ts
@@ -4,6 +4,10 @@ import { uploadFileError } from '../utils/uploadFileError';
 import type { IUser } from '../models/user.ts';
 import { getWSPermission } from '../utils/permsFunctions.ts';
 import { WSPermission } from '../models/profile.ts';
+import Workspace from '../schemas/workspaceSchema.ts';
+import { type IItem, ItemType } from '../models/item.ts';
+import { FileItem } from '../schemas/itemSchema.ts';
+import { Permission } from '../models/profilePerms.ts';
 
 const storage = multer.diskStorage({
     
@@ -18,38 +22,62 @@ const storage = multer.diskStorage({
       cb(null, dir);
     },
 
-    filename: (_, file, cb) => {
-      cb(null, file.originalname);
+    filename: async (req, file, cb) => {
+      const user: IUser = req.user as IUser;
+      const item = new FileItem({ name: file.originalname, path: req.body.path, itemType: ItemType.File, length: file.size, uploadDate: new Date(), modifiedDate: new Date(), profilePerms: [{ profile: user._id, permission: Permission.Owner }] });
+      const workspace = await Workspace.findOne({ _id: req.body.workspace }).populate('items');
+      await item.save();
+      workspace?.items.push(item.id);
+      await workspace?.save();
+      cb(null, item._id.toString());
     }
 
   });
   
-  export const uploader = multer({
-    storage,
-    preservePath: true,
-    fileFilter: async (req, file, cb) => {
-      const workspaceId: string = req.body.workspace;
-      const user: IUser = req.user as IUser;
-      const userId = user?._id;
-      if (workspaceId === undefined) {
-        cb(new uploadFileError('El campo workspace es requerido', 400));
-      }else{
-        if (userId === undefined) {
-            cb(new uploadFileError('El campo userId es requerido', 400));
-        } else{
-          const perm = await getWSPermission(userId, workspaceId)
-          if (perm === WSPermission.Read || perm === undefined) {
-            cb(new uploadFileError('No tienes permiso para subir archivos a este workspace.', 400));
-          } else {
-            if (file.originalname.includes('../')) {
-              cb(new uploadFileError('El nombre del archivo no puede contener "../"', 400));
-            } else {
-                cb(null, true); 
-            }
-        }
-      } 
-    }
+export const uploader = multer({
+  storage,
+  preservePath: true,
+  fileFilter: async (req, file, cb) => {
+    const error = await checkFile(req, file);
+    if (error) {
+      cb(new uploadFileError(error, 400));
+    } else {
+      cb(null, true);
+    } 
   }
 });
+
+const checkFile = async (req: any, file: any) => {
+  const wsId: string = req.body.workspace;
+  const path = req.body.path ? req.body.path : "";
+  const user: IUser = req.user as IUser;
+  const userId = user?._id;
+
+  const workspace = await Workspace.findOne({ _id: wsId }).populate('items');
+    if (!workspace) {
+      return 'No se ha encontrado el workspace';
+    }
+    if (userId === undefined) {
+        return 'El campo userId es requerido';
+    } 
+    const perm = await getWSPermission(userId, wsId)
+    if (perm === WSPermission.Read || perm === undefined) {
+      return 'No tienes permiso para subir archivos a este workspace.';
+    } 
+
+    const folders = path.split('/');
+    const folder = folders[folders.length - 1];
+    const existingFolder = (workspace.items as unknown as IItem[]).find((item: IItem) => item.name === folder && item.itemType === ItemType.Folder);
+    const folderPath = folders.slice(0, -1).join('/');
+    if (path != "" && (!existingFolder || existingFolder.path != folderPath)) {
+      return 'La ruta no es válida';
+    }
+
+    if (file.originalname.includes('../')) {
+      return 'El nombre del archivo no puede contener "../"';
+    }
+
+    return undefined; 
+  }
 
 
--- express_backend/src/controllers/workspaceController.ts
+++ express_backend/src/controllers/workspaceController.ts
@@ -2,12 +2,17 @@ import type { IProfile } from '../models/profile.ts';
 import { ItemType, type IItem } from '../models/item.ts';
 import { ProfileType, WSPermission } from '../models/profile.ts';
 import type { IProfilePerms } from '../models/profilePerms.ts';
+import { Permission } from '../models/profilePerms.ts';
+import type { IUser } from '../models/user.ts';
 import { CalendarItem, EventItem, FileItem, FolderItem, NoteItem, NoticeItem, StudySessionItem, TimerItem } from '../schemas/itemSchema.ts';
 import Workspace, { Profile } from '../schemas/workspaceSchema.ts';
-import { parseValidationError } from '../utils/errorParser.ts';
-import { Permission } from '../models/profilePerms.ts';
-import { getUserPermission, getWSPermission } from '../utils/permsFunctions.ts';
+import { Item } from '../schemas/itemSchema.ts';
 import User from '../schemas/userSchema.ts';
+import { parseValidationError } from '../utils/errorParser.ts';
+import { getUserPermission, getWSPermission } from '../utils/permsFunctions.ts';
+import mongoose from 'mongoose';
+import fs from 'fs';
+
 
 export const getWorkspace = async (req: any, res: any) => {
   try {
@@ -90,40 +95,6 @@ export const addUserToWorkspace = async (req: any, res: any) => {
 
   res.status(201).json(workspace);
   }catch (error: any) {
-    res.status(404).json({ error: error.message });
-  }
-  
-}
-
-export const addFileToWorkspace = async (req: any, res: any) => {
-  const wsId = req.body.workspace;
-  const path = req.body.path ? req.body.path : "";
-  const file = req.file;
-  const item = new FileItem({ name: file.originalname, path: path, itemType: ItemType.File, length: file.size, uploadDate: new Date(), modifiedDate: new Date(), profilePerms: [] });
-
-  try {
-    const workspace = await Workspace.findOne({ _id: wsId }).populate('items');
-    if (!workspace) {
-      res.status(404).json({ error: 'No se ha encontrado el workspace' });
-    }
-    else {
-
-      const folders = path.split('/');
-      const folder = folders[folders.length - 1];
-      const existingFolder = (workspace.items as unknown as IItem[]).find((item: IItem) => item.name === folder && item.itemType === ItemType.Folder);
-      const folderPath = folders.slice(0, -1).join('/');
-
-      if (path != "" && (!existingFolder || existingFolder.path != folderPath)) {
-        res.status(404).json({ error: 'No se ha encontrado la carpeta' });
-      } else {
-        await item.save();
-        workspace.items.push(item.id);
-        await workspace.save();
-        res.status(201).json(item);
-      }
-    }
-  }
-  catch (error: any) {
     res.status(404).json({ error: error.message });
   }
 }
@@ -243,6 +214,98 @@ export const changePerms = async (req: any, res: any) => {
     res.status(201).json(item);
   }
   catch (error: any) {
+    res.status(404).json({ error: error.message });
+  }
+}
+
+export const downloadFile = async (req: any, res: any) => 
+{
+  const wsId = req.query.workspace;
+  const fileId = req.query.fileId;
+  try {
+    const workspace = await Workspace.findOne({ _id: wsId });
+    if (!workspace) {
+      res.status(404).json({ error: 'No se ha encontrado el workspace' });
+      return;
+    }
+    const file = await FileItem.findOne({ _id: fileId });
+    if (!file) {
+      res.status(404).json({ error: 'No se ha encontrado el archivo' });
+      return;
+    }
+    const perm = await getUserPermission(req.user._id, wsId, fileId);
+    if (!perm) {
+      res.status(401).json({ error: 'No estás autorizado para ver este archivo' });
+      return;
+    }
+    if (fs.existsSync(`uploads/${wsId}/${fileId}`)) {
+        res.setHeader('Content-disposition', `attachment; filename=${file.name}`);
+        res.setHeader('Content-type', 'application/octet-stream');
+
+        const fileStream = fs.createReadStream(`uploads/${wsId}/${fileId}`);
+        fileStream.pipe(res);
+    } else {
+        res.status(404).json({ success: false, error: 'El archivo no existe'} );
+    }
+  }
+  catch (error: any) {
+    res.status(404).json({ error: error.message });
+  }
+}
+
+export const deleteItemFromWorkspace = async (req: any, res: any) => {
+  const wsId = req.body.workspace;
+  const fileId = req.body.fileId;
+  try {
+    const workspace = await Workspace.findOne({ _id: wsId }).populate('items');
+    if (!workspace) {
+      res.status(404).json({ error: 'No se ha encontrado el workspace' });
+      return;
+    }
+    const item = (workspace.items as unknown as IItem[]).find((item: IItem) => item._id.toString() === fileId);
+    if (!item) {
+      res.status(404).json({ error: 'No se ha encontrado el item' });
+      return;
+    }
+    const perm = await getUserPermission(req.user._id, wsId, fileId);
+    console.log(perm);
+    if (![Permission.Owner, Permission.Write].find(x => x == perm)){
+      res.status(401).json({ error: 'No estás autorizado para borrar este item' });
+      return;
+    }
+    (workspace.items as unknown as IItem[]) = (workspace.items as unknown as IItem[]).filter((item: IItem) => item._id.toString() !== fileId);
+    await Item.deleteOne({ _id: fileId });
+    await workspace.save();
+    fs.unlink(`uploads/${wsId}/${fileId}`, ()=>{});
+    res.status(200).json({ success: true });
+  } catch (error: any) {
+    res.status(404).json({ error: error.message });
+  }
+}
+
+export const toggleFavorite = async (req: any, res: any) => {
+  const wsId = req.body.workspace;
+  const { fileId } = req.body;
+  const user = req.user as IUser;
+  try {
+    const item = await FileItem.findOne({ _id: fileId });
+    if (!item) {
+      res.status(404).json({ error: 'No se ha encontrado el archivo' });
+      return;
+    }
+    const perm = await getUserPermission(user._id, wsId, fileId);
+    if (!perm) {
+      res.status(401).json({ error: 'No estás autorizado para ver este archivo' });
+      return;
+    }
+    const liked = user.favorites.includes(fileId);
+    user.favorites = user.favorites.filter((fav: mongoose.Types.ObjectId) => fav.toString() !== fileId);
+    user.favorites.push(fileId);
+    await user.save();
+
+    liked?res.json({ success: true, message: 'Archivo '+ fileId + ' marcado como favorito'}):res.json({ success: true, message: 'Archivo '+ fileId + ' desmarcado como favorito'});
+
+  } catch(error: any) {
     res.status(404).json({ error: error.message });
   }
 }
